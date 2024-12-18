acd34e502bcf822d5ca484bbc442a690efe9149c
Osama Alsalmi <osamaalsalmi_@outlook.com>
2024-12-18 17:56:22 +0300

    more config

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -242,6 +242,7 @@ require("lazy").setup({
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
   --
+  { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     "lewis6991/gitsigns.nvim",
@@ -357,7 +358,6 @@ require("lazy").setup({
       { "nvim-telescope/telescope-ui-select.nvim" },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -950,9 +950,9 @@ require("lazy").setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require "kickstart.plugins.debug",
   require "kickstart.plugins.indent_line",
-  -- require 'kickstart.plugins.lint',
+  require "kickstart.plugins.lint",
   require "kickstart.plugins.autopairs",
   require "kickstart.plugins.neo-tree",
   require "kickstart.plugins.gitsigns", -- adds gitsigns recommend keymaps
